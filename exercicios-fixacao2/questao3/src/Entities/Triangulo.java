75e6dc65c3815af5b4c033abacb87a9f5f6a9914
Gabriel Calado <gcalado501@gmail.com>
2023-09-12 16:23:58 -0300

    Exercicios Finalizados

---
+++ exercicios-fixacao2/questao3/src/Entities/Triangulo.java
@@ -0,0 +1,40 @@
+package Entities;
+
+public class Triangulo {
+    private double lado1;
+    private double lado2;
+    private double lado3;
+
+
+    public Triangulo(double lado1, double lado2, double lado3) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+
+    public double getLado1() {
+        return lado1;
+    }
+
+    public double getLado2() {
+        return lado2;
+    }
+
+    public double getLado3() {
+        return lado3;
+    }
+
+    public boolean formaTriangulo() {
+        return (lado1 + lado2 > lado3) && (lado1 + lado3 > lado2) && (lado2 + lado3 > lado1);
+    }
+
+    public String tipoTriangulo() {
+        if (lado1 == lado2 && lado2 == lado3) {
+            return "Equilátero";
+        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            return "Isósceles";
+        } else {
+            return "Escaleno";
+        }
+    }
+}
