effd8ca8e50fa3c7a2705276e84dcc85cc1f9365
Gabriel Calado <gcalado501@gmail.com>
2023-09-19 21:10:05 -0300

    Questão 1 exercicio 3 feita

--- exercicios-fixacao3/questao1/src/Entities/Combustivel.java
+++ exercicios-fixacao3/questao1/src/Entities/Combustivel.java
@@ -1,4 +1,31 @@
 package Entities;
 
 public class Combustivel {
+    private double litros;
+    private double alcool = 4.35;
+    private double gasolina = 5.6;
+
+
+    public Combustivel(double litros) {
+        this.litros = litros;
+    }
+
+    public double getLitros() {
+        return litros;
+    }
+
+    public double getAlcool() {
+        return alcool;
+    }
+
+    public double getGasolina() {
+        return gasolina;
+    }
+
+    public double totalAlcool() {
+        return alcool * litros;
+    }
+    public double totalGasolina(){
+        return gasolina * litros;
+    }
 }
--- exercicios-fixacao3/questao1/src/Main.java
+++ exercicios-fixacao3/questao1/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+import Entities.Combustivel;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escolha um tipo de combustivel: ");
+        System.out.println("A. Alcool");
+        System.out.println("G. Gasolina");
+        char combustivel = sc.next().charAt(0);
+        System.out.println();
+        System.out.print("Quantos Litros serão repostos: ");
+        double litros = sc.nextDouble();
+        Combustivel combustivel1 = new Combustivel(litros);
+
+
+        switch (combustivel){
+            case 'a':
+                System.out.printf("Você abasteceu %.2f litros de Alcool pelo valor de R$%.2f", litros, combustivel1.totalAlcool());
+                break;
+            case 'g':
+                System.out.printf("Você abasteceu %.2f litros de Gasolina pelo valor de R$%.2f", litros, combustivel1.totalGasolina());
+                break;
+            default:
+                System.out.println("Escolha uma opção valida");
+        }
+
+
+
+        sc.close();
     }
 }
