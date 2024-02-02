40e089d1bb0691ffdb46b72915628fda3c60d0e9
Gabriel Calado <gcalado501@gmail.com>
2024-02-02 14:38:40 -0300

    Fixation Exercise (Set)

--- studies/Seccao19/Fixation-Exercise/src/Application/Progam.java
+++ studies/Seccao19/Fixation-Exercise/src/Application/Progam.java
@@ -1,7 +1,40 @@
 package Application;
 
+import Models.Entities.Student;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Progam {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Set<Student> setStudent = new HashSet<>();
+
+        System.out.print("How many students for course A? ");
+        int courseA = sc.nextInt();
+        System.out.println("Students codes: ");
+        for (int i = 0; i < courseA ; i++) {
+            setStudent .add(new Student(sc.nextInt()));
+        }
+
+        System.out.print("How many students for course B? ");
+        int courseB = sc.nextInt();
+        System.out.println("Students codes: ");
+        for (int i = 0; i < courseB ; i++) {
+            setStudent .add(new Student(sc.nextInt()));
+        }
+
+        System.out.print("How many students for course C? ");
+        int courseC = sc.nextInt();
+        System.out.println("Students codes: ");
+        for (int i = 0; i < courseC ; i++) {
+            setStudent .add(new Student(sc.nextInt()));
+        }
+
+        System.out.println("Total students: " + setStudent.size());
 
     }
 }
---
+++ studies/Seccao19/Fixation-Exercise/src/Models/Entities/Student.java
@@ -0,0 +1,35 @@
+package Models.Entities;
+
+import java.util.Objects;
+
+public class Student {
+
+    private Integer code;
+
+    public Student(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        return Objects.equals(code, student.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return code != null ? code.hashCode() : 0;
+    }
+}
